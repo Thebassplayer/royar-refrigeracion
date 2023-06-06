ed867002bbd7a3a81f5c9ce76668eb503f870d75
Roy Lopez <roylopezdev@gmail.com>
2023-06-06 15:31:07 -0300

    adds AA washing service to AboutUs section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLocationDot,
   faScrewdriverWrench,
+  faShower,
 } from "@fortawesome/free-solid-svg-icons";
 
 const AboutUs = () => {
@@ -17,11 +18,11 @@ const AboutUs = () => {
       <h2 className="pt-0 text-center text-2xl font-extrabold tracking-wide  text-white sm:px-16 lg:text-3xl xl:px-48">
         Servicio técnico en Aires Acondicionados
       </h2>
-      {
-        // Description
-      }
       <div className="flex grow flex-col justify-evenly">
-        <div className="flex items-center rounded-lg bg-gray-700 p-4 shadow-lg">
+        {
+          // Description
+        }
+        <div className="mt-4 flex grow items-center rounded-lg bg-gray-700 px-4 py-2 shadow-lg">
           <div className="pr-4">
             <FontAwesomeIcon
               icon={faScrewdriverWrench}
@@ -30,13 +31,31 @@ const AboutUs = () => {
             />
           </div>
           <h3 className="text-white">
-            Especializados en Split y Piso-Techo. Reparación e instalación.
+            Especializados en equipos Split y Piso-Techo. Reparación e
+            instalación.
+          </h3>
+        </div>
+        {
+          //Lavamos tu aire
+        }
+        <div className="mt-4 flex grow items-center rounded-lg bg-gray-700 px-4 py-2 shadow-lg">
+          <div className="pr-4">
+            <FontAwesomeIcon
+              icon={faShower}
+              size="xl"
+              rotation={270}
+              style={{ color: "#35f500" }}
+            />
+          </div>
+          <h3 className="text-white">
+            Lavamos tu aire acondicionado. Mejora el funionamiento. Baja el
+            onsumo. Evita malos olores y bacterias.
           </h3>
         </div>
         {
           // Location
         }
-        <div className="flex items-center rounded-lg bg-gray-700 p-4 shadow-lg">
+        <div className="mt-4 flex grow items-center rounded-lg bg-gray-700 px-4 py-2 shadow-lg">
           <div className="pr-4">
             <FontAwesomeIcon
               icon={faLocationDot}
@@ -44,7 +63,7 @@ const AboutUs = () => {
               style={{ color: "#ff0000" }}
             />
           </div>
-          <h3 className="mt-2 text-sm font-normal text-white sm:px-16 lg:text-xl xl:px-48 ">
+          <h3 className="text-white">
             Nuñez, Belgrano, Villa Urquiza, Saavedra, Coghlan, Colegiales,
             Vicente Lopez, San Isidro
           </h3>
