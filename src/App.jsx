1b182c0e47a09090798c3b0ce51ef0cc5597861e
Roy Lopez <roylopezdev@gmail.com>
2023-06-07 21:06:58 -0300

    Add more pics

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ import image1 from "./assets/image1.jpeg";
 import image2 from "./assets/image2.jpeg";
 import image3 from "./assets/image3.jpeg";
 import image4 from "./assets/image4.png";
+import image5 from "./assets/image5.jpeg";
+import image6 from "./assets/image6.jpeg";
+import image7 from "./assets/image7.jpeg";
+import image8 from "./assets/image8.jpg";
 
 const App = () => {
   const [isCleaningVideo, setIscleaningVideo] = useState(false);
@@ -23,6 +27,10 @@ const App = () => {
     { id: 2, src: image2 },
     { id: 3, src: image3 },
     { id: 4, src: image4 },
+    { id: 5, src: image5 },
+    { id: 6, src: image6 },
+    { id: 7, src: image7 },
+    { id: 8, src: image8 },
   ];
 
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <section>
+    <section id="header">
       <h1 className="px-4 py-6 text-center text-3xl font-extrabold leading-none tracking-tight text-white dark:text-white sm:text-5xl md:py-8 md:text-5xl lg:py-4 lg:text-left lg:text-4xl">
         ❄️ Royal Refrigeracion
       </h1>
