5060f0dbd03f52974943b27e2dd6fbdacb4bc490
Roy Lopez <roylopezdev@gmail.com>
2023-06-12 15:31:14 -0300

    Add .env

--- src/client.js
+++ src/client.js
@@ -1,10 +1,14 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
+// Get Sanity project and dataset details
+const projectId = import.meta.env.VITE_SANITY_PROJECT_ID;
+const dataset = import.meta.env.VITE_SANITY_DATASET;
+
 // Sanity Client
 export const client = createClient({
-  projectId: "6w6t054p",
-  dataset: "production",
+  projectId: projectId,
+  dataset: dataset,
   apiVersion: "2021-03-25",
   useCdn: true,
 });
