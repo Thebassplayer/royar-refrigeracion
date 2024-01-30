752ff90b33c01ed359e197596df61f6daefc2b83
Roy Lopez <roylopezdev@gmail.com>
2024-01-30 14:25:15 -0300

    Update copyright year in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const Footer = (): JSX.Element => {
   return (
     <section className="mt-2 flex flex-col bg-gray-800 text-center text-xs text-gray-300 md:flex-row md:justify-evenly">
-      <p>© 2023 Refrigeracion Royal. All rights reserved.</p>
+      <p>© 2024 Refrigeracion Royal. All rights reserved.</p>
       <a href="mailto:royalservicearg@gmail.com">
         <p>royalservicearg@gmail.com</p>
       </a>
