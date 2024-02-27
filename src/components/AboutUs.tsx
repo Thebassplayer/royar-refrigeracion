ceb4e469bddaa45d94756a66871ffd12cd0e29b4
Roy Lopez <roylopezdev@gmail.com>
2024-02-27 14:53:02 -0300

    Refactor AboutUs component styling

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -34,7 +34,7 @@ const AboutUs = () => {
         <h2 className="mb-4 py-0 text-center text-2xl text-white sm:px-16 sm:text-3xl lg:text-3xl xl:px-48">
           ¿Quienes Somos?
         </h2>
-        <div className="container flex cursor-pointer flex-col gap-4 rounded-lg bg-gray-700 p-4 sm:text-xl md:flex-col lg:text-2xl">
+        <div className="container flex flex-col gap-4 rounded-lg bg-gray-700 p-4 sm:text-xl md:flex-col">
           {aboutUsContent.map(({ id, text }) => (
             <AboutUsElement key={id} content={text} />
           ))}
